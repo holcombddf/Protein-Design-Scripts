eeba4417581c807b0ee272eb0dfa0c0ebaafd3ff
David Holcomb <holcombddf@gmail.com>
2018-03-21 16:47:26 -0400

    Updated the schedulers to also include memory limits. They are still untested.

--- ad_hoc_scheduler.py
+++ ad_hoc_scheduler.py
@@ -1,27 +1,40 @@
- #!/usr/bin/python
-#This script will run a number of jobs, limited to LIM threads. As threads finish, it will run new jobs.
-#Can take an integer as a limit to the number of threads
+#!/usr/bin/python
+#This script will run a number of jobs, limited to TLIM threads and MLIM memory. As threads finish, it will run new jobs.
+#Can take integers as arguments as limits to the number of threads and the memory limit, respectively.
 #Change the GENERATE FILELIST HERE and GENERATE CMD HERE sections
 import sys
 import os
 import threading
-from threading import BoundedSemaphore
+import resource
+from threading import BoundedSemaphore, Lock
+#import psutil
 
 #runs the bash command
-def run_job(cmd, sem):
+def run_job(cmd, sem, lock):
   try:
     os.system(cmd)
   finally:
-    sem.release()
+    #release the lock, then the semaphore
+    try:
+      lock.release()
+    finally:
+      sem.release()
 
 def main(sysargv=[]):
-  #the maximum number of concurrent threads
-  LIM = 50
+  TLIM = 50 #the maximum number of concurrent threads
+  MLIM = 20000 #the memory limit
   if len(sysargv) > 0:
-    LIM = int(sysargv[0])
+    TLIM = int(sysargv[0])
+  if len(sysargv) > 1:
+    MLIM = int(sysargv[1])
+  
+  #imprecise safety precautions
+  TLIM = TLIM - 1
+  MLIM = MLIM * TLIM/(TLIM+1)
   
   t_arr = [] #array of threads
-  sem = BoundedSemaphore(value=LIM) #semaphore to control the number of threads
+  sem = BoundedSemaphore(value=TLIM) #semaphore to control the number of threads
+  lock = Lock()
   
   ##GENERATE FILELIST HERE
   filelist = []
@@ -32,11 +45,20 @@ def main(sysargv=[]):
     ##GENERATE CMD HERE
     cmd = "echo \"stuff\""
     
-    t_arr.append(threading.Thread(target=run_job, args=(cmd,sem,)))
+    t_arr.append(threading.Thread(target=run_job, args=(cmd,sem,lock,)))
     t_arr[i].daemon = True
     
     #wait until enough threads have finished to start another
     sem.acquire()
+    
+    #wait until there's enough memory
+    #process = psutil.Process(os.getpid())
+    #memuse = (process.memory_info()).rss
+    memdata = resource.getrusage(resource.RUSAGE_BOTH)
+    memuse = memdata.ru_ixrss + memdata.ru_idrss
+    if memuse >= MLIM:
+      lock.acquire(blocking=True)
+
     t_arr[i].start()
     
   #wait until the last thread is finished to exit
--- queue_scheduler.py
+++ queue_scheduler.py
@@ -1,29 +1,53 @@
- #!/usr/bin/python
-#This script will run a number of jobs, limited to LIM threads. As threads finish, it will run new jobs.
-#Can take an integer as a limit to the number of threads
+#!/usr/bin/python
+#This script will run a number of jobs, limited to TLIM threads and MLIM memory. As threads finish, it will run new jobs.
+#Can take integers as arguments as limits to the number of threads and the memory limit, respectively.
 #Change the GENERATE FILELIST HERE and GENERATE CMD HERE sections
 import sys
 import os
 import threading
+import resource
+from threading import Lock
 from Queue import Queue
+#import psutil
 
 #grabs a command from the queue, and runs it
-def worker(q):
+def worker(q,lock):
   while not q.empty():
+    #wait until there's enough memory
+    #process = psutil.Process(os.getpid())
+    #memuse = (process.memory_info()).rss
+    memdata = resource.getrusage(resource.RUSAGE_BOTH)
+    memuse = memdata.ru_ixrss + memdata.ru_idrss
+    if memuse >= MLIM:
+      lock.acquire(blocking=True)
+    
+    #get a command and run it
     cmd = q.get()
     try:
       os.system(cmd)
+    #free up the queue and (try to) release the lock
     finally:
       q.task_done()
+      try:
+	lock.release()
+      except Exception as e:
+	pass
 
 def main(sysargv=[]):
-  #the maximum number of concurrent threads
-  LIM = 50
+  TLIM = 50 #the maximum number of concurrent threads
+  MLIM = 20000 #the memory limit
   if len(sysargv) > 0:
-    LIM = int(sysargv[0])
+    TLIM = int(sysargv[0])
+  if len(sysargv) > 1:
+    MLIM = int(sysargv[1])
+    
+  #imprecise safety precautions
+  TLIM = TLIM - 1
+  MLIM = MLIM * TLIM/(TLIM+1)
   
   t_arr = [] #array of threads
   q = Queue() #queue of commands
+  lock = Lock()
   
   ##GENERATE FILELIST HERE
   filelist = []
@@ -36,9 +60,9 @@ def main(sysargv=[]):
 
     q.put(cmd)
   
-  #create LIM workers to process the queue
-  for i in range(LIM):
-    t_arr.append(threading.Thread(target=worker, args=(q,)))
+  #create TLIM workers to process the queue
+  for i in range(TLIM):
+    t_arr.append(threading.Thread(target=worker, args=(q,lock,)))
     t_arr[i].daemon = True
     t_arr[i].start()
     
